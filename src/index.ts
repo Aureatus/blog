4bb1b3033de4bae00ec79e55050e1141bf792f6c
Alex Knott <alextknott@gmail.com>
2022-09-19 16:42:27 +0100

    Initial commit with an express typescript app

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import morgan from 'morgan';
+import createError from "http-errors";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("Express + TypeScript Server TEST");
+});
+
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
