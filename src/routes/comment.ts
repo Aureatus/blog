9255601dc7870d760f799c9d0538ecdd4f8b6b75
Alex Knott <alextknott@gmail.com>
2022-09-21 13:34:07 +0100

    Use imported comment controller functions instead of placeholder function

--- src/routes/comment.ts
+++ src/routes/comment.ts
@@ -1,13 +1,15 @@
 import { Request, Response, Router } from "express";
 
+import { commentListGet, commentCreate } from "../controllers/comment";
+
 const commentRouter = Router();
 
 const placeHolderResponse = (req: Request, res: Response) => {
   res.send("test");
 };
 
-commentRouter.get("/blogs/:id", placeHolderResponse); // Return all comments where post matches provided id.
+commentRouter.get("/blogs/:id", commentListGet); // Return all comments where post matches provided id.
 
-commentRouter.post("/create/:id", placeHolderResponse); // Create comment for post of provided id if user is logged in.
+commentRouter.post("/create/:id", commentCreate); // Create comment for post of provided id if user is logged in.
 
 export default commentRouter;
