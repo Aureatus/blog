e32dbcaff8a82618210297780181bc7eeb6019fc
Alex Knott <alextknott@gmail.com>
2022-09-21 13:30:15 +0100

    Use imported blog controller functions instead of placeholder function

--- src/routes/blog.ts
+++ src/routes/blog.ts
@@ -1,19 +1,27 @@
 import { Request, Response, Router } from "express";
 
+import {
+  blogListGet,
+  blogDetailGet,
+  blogUpdate,
+  blogCreate,
+  blogDelete,
+} from "../controllers/blog";
+
 const blogRouter = Router();
 
 const placeHolderResponse = (req: Request, res: Response) => {
   res.send("test");
 };
 
-blogRouter.get("/", placeHolderResponse); // Return published blogs with title, author and timestamp.
+blogRouter.get("/", blogListGet); // Return published blogs with title, author and timestamp.
 
-blogRouter.get("/:id", placeHolderResponse); // Return blog of provided id with title, content, author and timestamp.
+blogRouter.get("/:id", blogDetailGet); // Return blog of provided id with title, content, author and timestamp.
 
-blogRouter.put("/:id/update", placeHolderResponse); // Update  blog of provided id if user is same as blog author or is admin.
+blogRouter.put("/:id/update", blogUpdate); // Update  blog of provided id if user is same as blog author or is admin.
 
-blogRouter.post("/create", placeHolderResponse); // Create blog if user is logged in.
+blogRouter.post("/create", blogCreate); // Create blog if user is logged in.
 
-blogRouter.post("/:id/delete", placeHolderResponse); // Delete blog of provided id if user is same as blog author or is admin.
+blogRouter.post("/:id/delete", blogDelete); // Delete blog of provided id if user is same as blog author or is admin.
 
 export default blogRouter;
