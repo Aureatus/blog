eeba6816a37ba4d7581534ec7911fc4879698b5f
Alex Knott <alextknott@gmail.com>
2022-09-21 15:41:36 +0100

    Hash password before saving users

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,5 @@
 import { Schema, model } from "mongoose";
+import { hash } from "bcrypt";
 
 const UserSchema = new Schema({
   user_name: { type: String, required: true },
@@ -6,6 +7,12 @@ const UserSchema = new Schema({
   family_name: { type: String, required: true },
   password: { type: String, required: true },
   admin: { type: Boolean, required: true },
+});
+
+UserSchema.pre("save", async function encryptPassword(next) {
+  const hashedPassword = await hash(this.password, 10);
+  this.password = hashedPassword;
+  next();
 });
 
 const User = model("User", UserSchema);
