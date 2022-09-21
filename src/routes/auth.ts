bd4c515916ecf62695fda06cb0d1d3a967315a99
Alex Knott <alextknott@gmail.com>
2022-09-21 13:23:48 +0100

    Remove unused placeholder function and imports it needed

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,12 +1,8 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 
 import { loginPost, logoutPost, signupPost } from "../controllers/auth";
 
 const authRouter = Router();
-
-const placeHolderResponse = (req: Request, res: Response) => {
-  res.send("test");
-};
 
 authRouter.post("/login", loginPost);
 
