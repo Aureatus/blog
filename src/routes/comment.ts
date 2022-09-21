850a507bf2bb89812516801e9f47fd74b309d601
Alex Knott <alextknott@gmail.com>
2022-09-21 13:34:39 +0100

    Remove unused placeholder function and imports it needed

--- src/routes/comment.ts
+++ src/routes/comment.ts
@@ -1,12 +1,8 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 
 import { commentListGet, commentCreate } from "../controllers/comment";
 
 const commentRouter = Router();
-
-const placeHolderResponse = (req: Request, res: Response) => {
-  res.send("test");
-};
 
 commentRouter.get("/blogs/:id", commentListGet); // Return all comments where post matches provided id.
 
