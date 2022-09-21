13de76fd85f725065c78e2990db00a3a111df4b8
Alex Knott <alextknott@gmail.com>
2022-09-21 13:23:20 +0100

    Use imported auth controller functions instead of placeholder function

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,15 +1,17 @@
 import { Request, Response, Router } from "express";
 
+import { loginPost, logoutPost, signupPost } from "../controllers/auth";
+
 const authRouter = Router();
 
 const placeHolderResponse = (req: Request, res: Response) => {
   res.send("test");
 };
 
-authRouter.post("/login", placeHolderResponse);
+authRouter.post("/login", loginPost);
 
-authRouter.post("/logout", placeHolderResponse);
+authRouter.post("/logout", logoutPost);
 
-authRouter.post("/signup", placeHolderResponse);
+authRouter.post("/signup", signupPost);
 
 export default authRouter;
