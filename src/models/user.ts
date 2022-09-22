c23bd8fe5f83c5691b95804eae7802f39be9d580
Alex Knott <alextknott@gmail.com>
2022-09-22 17:24:21 +0100

    Assign function through schema options so that separate TS type isn't needed

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,26 +1,29 @@
 import { Schema, model } from "mongoose";
 import { compare, hash } from "bcrypt";
 
-const UserSchema = new Schema({
-  user_name: { type: String, required: true },
-  given_name: { type: String, required: true },
-  family_name: { type: String, required: true },
-  password: { type: String, required: true },
-  admin: { type: Boolean, required: true },
-});
+const UserSchema = new Schema(
+  {
+    user_name: { type: String, required: true },
+    given_name: { type: String, required: true },
+    family_name: { type: String, required: true },
+    password: { type: String, required: true },
+    admin: { type: Boolean, required: true },
+  },
+  {
+    methods: {
+      async isValidPassword(password: string) {
+        const passwordValidity = await compare(password, this.password);
+        return passwordValidity;
+      },
+    },
+  }
+);
 
 UserSchema.pre("save", async function encryptPassword(next) {
   const hashedPassword = await hash(this.password, 10);
   this.password = hashedPassword;
   next();
 });
-
-UserSchema.methods.isValidPassword = async function validatePass(
-  password: string
-) {
-  const passwordValidity = await compare(password, this.password);
-  return passwordValidity;
-};
 const User = model("User", UserSchema);
 
 export default User;
