753e80fc6efea9aebf4d0d68a12c32f0f03b989b
Alex Knott <alextknott@gmail.com>
2022-09-23 21:30:06 +0100

    Update route to create comment

--- src/routes/comment.ts
+++ src/routes/comment.ts
@@ -9,6 +9,6 @@ const commentRouter = Router();
 
 commentRouter.get("/blogs/:id", commentListGet); // Return all comments where post matches provided id.
 
-commentRouter.post("/create/:id", protectedRoute, commentCreate); // Create comment for post of provided id if user is logged in.
+commentRouter.post("/:id/create/", protectedRoute, commentCreate); // Create comment for post of provided id if user is logged in.
 
 export default commentRouter;
