f8d0c7ca543b2a579611332a8bd03f6e38aca6c6
Alex Knott <alextknott@gmail.com>
2022-09-23 13:43:12 +0100

    Create protected routes for updating, creating and deleting blogs

--- src/routes/blog.ts
+++ src/routes/blog.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import passport from "passport";
 
 import {
   blogListGet,
@@ -8,16 +9,18 @@ import {
   blogDelete,
 } from "../controllers/blog";
 
+const protectedRoute = passport.authenticate("jwt", { session: false });
+
 const blogRouter = Router();
 
 blogRouter.get("/", blogListGet); // Return published blogs with title, author and timestamp.
 
 blogRouter.get("/:id", blogDetailGet); // Return blog of provided id with title, content, author and timestamp.
 
-blogRouter.put("/:id/update", blogUpdate); // Update  blog of provided id if user is same as blog author or is admin.
+blogRouter.put("/:id/update", protectedRoute, blogUpdate); // Update  blog of provided id if user is same as blog author or is admin.
 
-blogRouter.post("/create", blogCreate); // Create blog if user is logged in.
+blogRouter.post("/create", protectedRoute, blogCreate); // Create blog if user is logged in.
 
-blogRouter.post("/:id/delete", blogDelete); // Delete blog of provided id if user is same as blog author or is admin.
+blogRouter.post("/:id/delete", protectedRoute, blogDelete); // Delete blog of provided id if user is same as blog author or is admin.
 
 export default blogRouter;
