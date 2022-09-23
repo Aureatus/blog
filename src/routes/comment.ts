c705ba417590c9184994babf7e42d62203ee2042
Alex Knott <alextknott@gmail.com>
2022-09-23 13:44:04 +0100

    Create protected route and use it to protect creation of comments

--- src/routes/comment.ts
+++ src/routes/comment.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
+import passport from "passport";
 
 import { commentListGet, commentCreate } from "../controllers/comment";
+
+const protectedRoute = passport.authenticate("jwt", { session: false });
 
 const commentRouter = Router();
 
 commentRouter.get("/blogs/:id", commentListGet); // Return all comments where post matches provided id.
 
-commentRouter.post("/create/:id", commentCreate); // Create comment for post of provided id if user is logged in.
+commentRouter.post("/create/:id", protectedRoute, commentCreate); // Create comment for post of provided id if user is logged in.
 
 export default commentRouter;
