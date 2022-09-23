00e6893d4780fa295325bacfd8cd86ad1904c8b3
Alex Knott <alextknott@gmail.com>
2022-09-23 21:30:23 +0100

    Implement ability to create comments on a blog

--- src/controllers/comment.ts
+++ src/controllers/comment.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import Comment from "../models/comment";
 
 const commentListGet = async (
   req: Request,
@@ -12,16 +14,31 @@ const commentListGet = async (
   }
 };
 
-const commentCreate = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    return res.send("test");
-  } catch (err) {
-    return next(err);
-  }
-};
+const commentCreate = [
+  body("text").isString().notEmpty().trim().escape(),
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(401).send(errors);
+      }
+
+      const { _id }: any = req.user;
+
+      const comment = new Comment({
+        text: req.body.text,
+        timestamp: Date(),
+        // eslint-disable-next-line no-underscore-dangle
+        author: _id,
+        post: req.params.id,
+      });
+
+      await Comment.create(comment);
+      return res.sendStatus(200);
+    } catch (err) {
+      return next(err);
+    }
+  },
+];
 
 export { commentListGet, commentCreate };
