38c5d2d9ed6fb8246fee475949ff371b4b6ee800
Alex Knott <alextknott@gmail.com>
2022-09-26 19:17:27 +0100

    Create function that fetches all comments for a given blog

---
+++ src/lib/fetch/fetchComments.ts
@@ -0,0 +1,11 @@
+const fetchComments = async (id: string) => {
+  try {
+    const response = await fetch(`http://localhost:3000/comments/blog/${id}`);
+    return await response.json();
+  } catch (err) {
+    if (err instanceof Error) return err.message;
+    return "Unknown error";
+  }
+};
+
+export default fetchComments;
