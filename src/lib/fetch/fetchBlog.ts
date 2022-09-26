203d3cb36f8af474c169fe81571c21d71a6a82ab
Alex Knott <alextknott@gmail.com>
2022-09-26 18:33:20 +0100

    Create function that fetches specific blog with all details from blog API

---
+++ src/lib/fetch/fetchBlog.ts
@@ -0,0 +1,11 @@
+const fetchBlog = async (id: string) => {
+  try {
+    const response = await fetch(`http://localhost:3000/blogs/${id}`);
+    return await response.json();
+  } catch (err) {
+    if (err instanceof Error) return err.message;
+    return "Unknown error";
+  }
+};
+
+export default fetchBlog;
