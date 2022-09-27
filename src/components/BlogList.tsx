fb893bf3f5e537abd536d4f00ed6546e78f76681
Alex Knott <alextknott@gmail.com>
2022-09-27 13:29:23 +0100

    Create interface for blog

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -1,6 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import getBlogList from "../lib/fetch/getBlogList";
 
+interface BlogInterface {
+  title: string;
+  timestamp: Date;
+  author: string;
+  published: boolean;
+  _id: string;
+}
+
 const BlogList = () => {
   const { data, isLoading, isError, error } = useQuery(["blogs"], getBlogList);
 
