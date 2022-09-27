598d487f5b98dd3b5f0657d7ad32fe7396ce16b9
Alex Knott <alextknott@gmail.com>
2022-09-27 18:36:26 +0100

    Render BlogInfo and CommentList with blogId as props
    Get blogId from url using react router loader

--- src/components/BlogDetail/BlogDetail.tsx
+++ src/components/BlogDetail/BlogDetail.tsx
@@ -1,5 +1,17 @@
+import { useLoaderData } from "react-router-dom";
+import BlogInfo from "./BlogInfo";
+import CommentList from "./CommentList";
+
 const BlogDetail = () => {
-  return <div></div>;
+  const blogId = useLoaderData();
+  if (typeof blogId !== "string") return <p>Invalid id type</p>;
+
+  return (
+    <div>
+      <BlogInfo blogId={blogId} />
+      <CommentList blogId={blogId} />
+    </div>
+  );
 };
 
 export default BlogDetail;
