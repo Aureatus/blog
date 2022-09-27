f3123829a5734244af6e298de2921fdd0df2716d
Alex Knott <alextknott@gmail.com>
2022-09-27 16:26:49 +0100

    Create BrowserRouter and pass it to RouterProvider
    Include index path that navigates to /blog

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,25 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 
 const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    index: true,
+    element: <Navigate to="/blogs" replace />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>
 );
