49be6041004ba8f4ab0afc5f236c1133a8f61bd9
Alex Knott <alextknott@gmail.com>
2022-09-27 13:41:33 +0100

    Add handlers for loading and errors

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -12,6 +12,11 @@ interface BlogInterface {
 const BlogList = () => {
   const { data, isLoading, isError, error } = useQuery(["blogs"], getBlogList);
 
+  if (isLoading) return null;
+  if (isError && error instanceof Error) return <p>{error.message}</p>;
+
+  if (typeof data === "string") return <p>{data}</p>;
+
   return <div />;
 };
 
