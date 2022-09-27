e9e0445ebbca294d081e15dcf050d049b46bc257
Alex Knott <alextknott@gmail.com>
2022-09-27 11:52:20 +0100

    Fetch list of blogs and loading + error information from blog API using react query

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -1,4 +1,9 @@
+import { useQuery } from "@tanstack/react-query";
+import getBlogList from "../lib/fetch/getBlogList";
+
 const BlogList = () => {
+  const { data, isLoading, isError, error } = useQuery(["blogs"], getBlogList);
+
   return <div />;
 };
 
