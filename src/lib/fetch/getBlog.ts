9d91831391672f304a9cf9f9cae4f9de22407b89
Alex Knott <alextknott@gmail.com>
2022-09-27 12:38:05 +0100

    Add error handling for HTTP responses that aren't 200

--- src/lib/fetch/getBlog.ts
+++ src/lib/fetch/getBlog.ts
@@ -1,6 +1,8 @@
 const getBlog = async (id: string) => {
   try {
     const response = await fetch(`http://localhost:3000/blogs/${id}`);
+    if (response.status !== 200)
+      throw new Error(`${response.status} ${response.statusText}`);
     return await response.json();
   } catch (err) {
     if (err instanceof Error) return err.message;
