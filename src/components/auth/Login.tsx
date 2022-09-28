b7769d6550a2631601a91c44c5faa4bbad81aaab
Alex Knott <alextknott@gmail.com>
2022-09-28 17:41:27 +0100

    Create barebones Login functionality with no validation/sanitisation

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,5 +1,46 @@
+import { useState, FormEvent } from "react";
+import postLogin from "../../lib/fetch/auth/postLogin";
+
 const Login = () => {
-  return <div></div>;
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const loginResponse = await postLogin(userName, password);
+      const bearerToken = await loginResponse.token;
+      localStorage.setItem("bearerToken", bearerToken);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={(e) => login(e)}>
+        <label htmlFor="userName">
+          Username
+          <input
+            type="text"
+            id="userName"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
 };
 
 export default Login;
