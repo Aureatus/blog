8f614cc7d28e6012e98fc23b727fb742b442379e
Alex Knott <alextknott@gmail.com>
2022-10-01 17:04:38 +0100

    Render CommentForm if user fetched from localStorage exists

--- src/components/BlogDetail/BlogDetail.tsx
+++ src/components/BlogDetail/BlogDetail.tsx
@@ -1,15 +1,19 @@
 import { useLoaderData } from "react-router-dom";
 import BlogInfo from "./BlogInfo";
+import CommentForm from "./CommentForm";
 import CommentList from "./CommentList";
 
 const BlogDetail = () => {
   const blogId = useLoaderData();
   if (typeof blogId !== "string") return <p>Invalid id type</p>;
 
+  const user = localStorage.getItem("bearerToken");
+
   return (
     <div>
       <BlogInfo blogId={blogId} />
       <CommentList blogId={blogId} />
+      {user ? <CommentForm /> : null}
     </div>
   );
 };
