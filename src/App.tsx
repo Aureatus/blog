367b49517032f08afbed19cb696a32ec617dfae1
Alex Knott <alextknott@gmail.com>
2022-10-03 15:31:18 +0100

    Move router creation into component so I can pass stateful props to elements in it

--- src/App.tsx
+++ src/App.tsx
@@ -10,38 +10,38 @@ import BlogDetail from "./components/BlogDetail/BlogDetail";
 import BlogHeader from "./components/BlogHeader";
 import BlogList from "./components/BlogList";
 
-const router = createBrowserRouter([
-  {
-    index: true,
-    element: <Navigate to="/blogs" replace />,
-  },
-  {
-    path: "/blogs",
-    element: (
-      <>
-        <BlogHeader />
-        <BlogList />
-      </>
-    ),
-  },
-  {
-    path: "/blogs/:blogId",
-    element: (
-      <>
-        <BlogHeader />
-        <BlogDetail />
-      </>
-    ),
-    loader: ({ params }) => params.blogId,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-]);
-
 const App = () => {
   const queryClient = new QueryClient();
+
+  const router = createBrowserRouter([
+    {
+      index: true,
+      element: <Navigate to="/blogs" replace />,
+    },
+    {
+      path: "/blogs",
+      element: (
+        <>
+          <BlogHeader />
+          <BlogList />
+        </>
+      ),
+    },
+    {
+      path: "/blogs/:blogId",
+      element: (
+        <>
+          <BlogHeader />
+          <BlogDetail />
+        </>
+      ),
+      loader: ({ params }) => params.blogId,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
 
   return (
     <QueryClientProvider client={queryClient}>
