b54ff9ee5a36b36157b831ae1ae862e2e7939180
Alex Knott <alextknott@gmail.com>
2022-10-03 19:45:24 +0100

    Create basic form and ability to create comments on submit

--- src/components/BlogDetail/CommentForm.tsx
+++ src/components/BlogDetail/CommentForm.tsx
@@ -1,9 +1,33 @@
 import { useState } from "react";
+import postComment from "../../lib/fetch/postComment";
 
-const CommentForm = () => {
+const CommentForm = ({ user, blogId }: { user: string; blogId: string }) => {
   const [commentText, setCommentText] = useState("");
 
-  return <div>asdasd</div>;
+  const createComment = async () => {
+    postComment(blogId, commentText, user);
+  };
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        createComment();
+      }}
+    >
+      <label htmlFor="commentText">
+        <input
+          type="text"
+          id="commentText"
+          placeholder="Type comment"
+          value={commentText}
+          onChange={(e) => {
+            setCommentText(e.target.value);
+          }}
+        />
+      </label>
+    </form>
+  );
 };
 
 export default CommentForm;
