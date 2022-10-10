6033b29b985be6915f66b924660a254558e1f110
Alex Knott <alextknott@gmail.com>
2022-10-10 17:22:52 +0100

    Create function that sends post signup request with provided values and returns response

---
+++ src/lib/fetch/auth/postSignUp.ts
@@ -0,0 +1,33 @@
+const postSignUp = async (
+  user_name: string,
+  given_name: string,
+  family_name: string,
+  password: string,
+  confirm_password: string
+) => {
+  const signUpDetails = {
+    user_name,
+    given_name,
+    family_name,
+    password,
+    confirm_password,
+  };
+
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams(signUpDetails),
+  };
+  try {
+    const response = await fetch(`http://localhost:3000/signup`, options);
+    if (!response.ok) throw new Error(` ${await response.text()}`);
+    return response.status;
+  } catch (err) {
+    if (err instanceof Error) return err;
+    return "Unknown error";
+  }
+};
+
+export default postSignUp;
