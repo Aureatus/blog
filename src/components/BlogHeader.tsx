c9c6ff01aa2c53d3e7e8c467bb9c0a80accb10ad
Alex Knott <alextknott@gmail.com>
2022-10-11 16:33:38 +0100

    Add link to signup page if user not logged in

--- src/components/BlogHeader.tsx
+++ src/components/BlogHeader.tsx
@@ -5,7 +5,14 @@ import Logout from "./auth/LogoutButton";
 const BlogHeader = ({ user, setUser }: UserStateInterface) => (
   <header>
     <h1>Blog project</h1>
-    {user ? <Logout setUser={setUser} /> : <Link to="/login">Login</Link>}
+    {user ? (
+      <Logout setUser={setUser} />
+    ) : (
+      <>
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Signup</Link>
+      </>
+    )}
   </header>
 );
 
