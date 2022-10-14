5a2a4a40d0310413a5d0de859b8150172d613b97
Alex Knott <alextknott@gmail.com>
2022-10-14 21:56:23 +0100

    Give comments basic styling

--- src/components/BlogDetail/CommentList.tsx
+++ src/components/BlogDetail/CommentList.tsx
@@ -24,20 +24,26 @@ const CommentList = ({ blogId }: { blogId: string }) => {
   if (isError && error instanceof Error) return <p>{error.message}</p>;
 
   return (
-    <>
+    <div className="columns is-multiline is-centered is-vcentered p-4">
       {data.map((comment: CommentInterface) => {
         // eslint-disable-next-line @typescript-eslint/naming-convention
         const { text, timestamp, author, _id } = comment;
 
         return (
-          <div key={_id}>
-            <h2>{`${author.given_name} ${author.family_name}`}</h2>
-            <p>{text}</p>
-            <p>{new Date(timestamp).toDateString()}</p>
+          <div key={_id} className="column column is-one-third">
+            <div className="message is-primary">
+              <div className="message-header">
+                <h2 className="is-size-4">{`${author.given_name} ${author.family_name}`}</h2>
+                <p>{new Date(timestamp).toDateString()}</p>
+              </div>
+              <div className="message-body">
+                <p className="subtitle">{text}</p>
+              </div>
+            </div>
           </div>
         );
       })}
-    </>
+    </div>
   );
 };
 
