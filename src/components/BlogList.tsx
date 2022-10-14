3fcc9f478e5fb7a7a93c6bbe81e70a227d5f44f9
Alex Knott <alextknott@gmail.com>
2022-10-14 20:52:52 +0100

    Wrap columns in a bulma box

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -25,35 +25,37 @@ const BlogList = () => {
   if (typeof data === "string") return <p>{data}</p>;
 
   return (
-    <div className="columns is-multiline is-centered is-vcentered p-4">
-      {data.map((blog: BlogInterface) => {
-        // eslint-disable-next-line @typescript-eslint/naming-convention
-        const { title, timestamp, author, published, _id } = blog;
+    <div className="box p-4 m-5">
+      <div className="columns is-multiline is-centered is-vcentered p-4">
+        {data.map((blog: BlogInterface) => {
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          const { title, timestamp, author, published, _id } = blog;
 
-        if (!published) return null;
+          if (!published) return null;
 
-        return (
-          <Link key={_id} to={_id} className="column is-one-third">
-            <div className="card">
-              <header className="card-header has-background-primary	">
-                <p className="card-header-title title is-3">{title}</p>
-              </header>
-              <div className="card-content">
-                <div className="content">
+          return (
+            <Link key={_id} to={_id} className="column is-one-third">
+              <div className="card">
+                <header className="card-header has-background-primary	">
+                  <p className="card-header-title title is-3">{title}</p>
+                </header>
+                <div className="card-content">
+                  <div className="content">
+                    <p className="subtitle is-5">
+                      Written by{" "}
+                      <strong>{`${author.given_name} ${author.family_name}`}</strong>
+                    </p>
+                  </div>
                   <p className="subtitle is-5">
-                    Written by{" "}
-                    <strong>{`${author.given_name} ${author.family_name}`}</strong>
+                    Published on:{" "}
+                    <strong>{new Date(timestamp).toDateString()}</strong>
                   </p>
                 </div>
-                <p className="subtitle is-5">
-                  Published on:{" "}
-                  <strong>{new Date(timestamp).toDateString()}</strong>
-                </p>
               </div>
-            </div>
-          </Link>
-        );
-      })}
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 };
