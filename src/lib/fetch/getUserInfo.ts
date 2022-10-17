9ddd19932bc2bfc8e294fb21048220168f0112df
Alex Knott <alextknott@gmail.com>
2022-10-17 13:15:20 +0100

    Create function that fetches user info via their bearer token

---
+++ src/lib/fetch/getUserInfo.ts
@@ -0,0 +1,18 @@
+const getUserInfo = async (bearerToken: string) => {
+  const options = {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: bearerToken,
+    },
+  };
+  try {
+    const response = await fetch(`http://localhost:3000/user/info`, options);
+    if (response.status !== 200) throw new Error(` ${await response.text()}`);
+    return await response.json();
+  } catch (err) {
+    if (err instanceof Error) return err.message;
+    return "Unknown error";
+  }
+};
+
+export default getUserInfo;
