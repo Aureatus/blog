caac1292d0db3a5aa63770ad089a713b424cd123
Alex Knott <alextknott@gmail.com>
2022-10-19 15:56:53 +0100

    Add link to create posts

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -22,44 +22,52 @@ const BlogList = ({ user }: UserStateInterface) => {
   const { _id: userId } = userData;
 
   return (
-    <div className="box p-4 m-5">
-      <div className="columns is-multiline is-centered is-vcentered p-4">
-        {data.map((blog: BlogDataInterface) => {
-          // eslint-disable-next-line @typescript-eslint/naming-convention
-          const { title, timestamp, author, _id } = blog;
-          // eslint-disable-next-line no-underscore-dangle
-          if (author._id !== userId) return null;
-
-          return (
-            <div key={_id} className="column is-one-third">
-              <div className="card">
-                <header className="card-header has-background-primary	">
-                  <p className="card-header-title title is-3">{title}</p>
-                </header>
-                <div className="card-content">
-                  <div className="content">
-                    <p className="subtitle is-5">
-                      Written by{" "}
-                      <strong>{`${author.given_name} ${author.family_name}`}</strong>
-                    </p>
-                    <p className="subtitle is-5">
-                      Published on:{" "}
-                      <strong>{new Date(timestamp).toDateString()}</strong>
-                    </p>
+    <div>
+      <div className="level">
+        <div className="level-item">
+          <Link className="button is-info" to="create">
+            Create post
+          </Link>
+        </div>
+      </div>
+      <div className="box p-4 m-5">
+        <div className="columns is-multiline is-centered is-vcentered p-4">
+          {data.map((blog: BlogDataInterface) => {
+            // eslint-disable-next-line @typescript-eslint/naming-convention
+            const { title, timestamp, author, _id } = blog;
+            // eslint-disable-next-line no-underscore-dangle
+            if (author._id !== userId) return null;
+            return (
+              <div key={_id} className="column is-one-third">
+                <div className="card">
+                  <header className="card-header has-background-primary	">
+                    <p className="card-header-title title is-3">{title}</p>
+                  </header>
+                  <div className="card-content">
+                    <div className="content">
+                      <p className="subtitle is-5">
+                        Written by{" "}
+                        <strong>{`${author.given_name} ${author.family_name}`}</strong>
+                      </p>
+                      <p className="subtitle is-5">
+                        Published on:{" "}
+                        <strong>{new Date(timestamp).toDateString()}</strong>
+                      </p>
+                    </div>
                   </div>
+                  <footer className="card-footer">
+                    <Link to={`${_id}/edit`} className="card-footer-item">
+                      Edit
+                    </Link>
+                    <Link to={`${_id}/delete`} className="card-footer-item ">
+                      Delete
+                    </Link>
+                  </footer>
                 </div>
-                <footer className="card-footer">
-                  <Link to={`${_id}/edit`} className="card-footer-item">
-                    Edit
-                  </Link>
-                  <Link to={`${_id}/delete`} className="card-footer-item ">
-                    Delete
-                  </Link>
-                </footer>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
