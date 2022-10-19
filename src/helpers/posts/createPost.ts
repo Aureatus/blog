aa8f646049988980c1928eba1f2852eeace89bdf
Alex Knott <alextknott@gmail.com>
2022-10-19 16:52:53 +0100

    Create helper function that calls postPost and manages state for error and success

---
+++ src/helpers/posts/createPost.ts
@@ -0,0 +1,37 @@
+import { Dispatch, SetStateAction } from "react";
+import PostResponseErrorInterface from "../../interfaces/PostResponseErrorInterface";
+import postPost from "../../lib/fetch/postPost";
+import findErrorObject from "../../lib/general/findErrorObject";
+
+const createPost = async (
+  title: string,
+  content: string,
+  published: boolean,
+  bearerToken: string,
+  setSuccess: Dispatch<SetStateAction<boolean>>,
+  setTitleError: Dispatch<SetStateAction<PostResponseErrorInterface | null>>,
+  setContentError: Dispatch<SetStateAction<PostResponseErrorInterface | null>>,
+  setPublishedError: Dispatch<SetStateAction<PostResponseErrorInterface | null>>
+) => {
+  try {
+    setSuccess(false);
+    const createResponse = await postPost(
+      title,
+      content,
+      published,
+      bearerToken
+    );
+    if (createResponse instanceof Error) throw createResponse;
+    setSuccess(true);
+  } catch (err) {
+    if (err instanceof Error) {
+      const errorArray = JSON.parse(err.message).errors;
+
+      setTitleError(findErrorObject(errorArray, "title"));
+      setContentError(findErrorObject(errorArray, "content"));
+      setPublishedError(findErrorObject(errorArray, "published"));
+    }
+  }
+};
+
+export default createPost;
