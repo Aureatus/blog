0c0e548bcf2e9531fac718713d1952157f2fa63e
Alex Knott <alextknott@gmail.com>
2022-10-20 22:40:58 +0100

    Create basic LoadingElement

---
+++ src/components/LoadingElement.tsx
@@ -0,0 +1,11 @@
+const LoadingElement = () => (
+  <div className="hero">
+    <div className="hero-body">
+      <progress className="progress is-small is-primary" max="100">
+        ?%
+      </progress>
+    </div>
+  </div>
+);
+
+export default LoadingElement;
