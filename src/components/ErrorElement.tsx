28b9fc43cccc775f2bb58833f44e2907385430ca
Alex Knott <alextknott@gmail.com>
2022-10-20 13:01:13 +0100

    Allow for error prop to be passed in, so it can be used for non router errors

--- src/components/ErrorElement.tsx
+++ src/components/ErrorElement.tsx
@@ -1,6 +1,28 @@
 import { Link, useRouteError } from "react-router-dom";
 
-const ErrorElement = () => {
+const ErrorElement = ({ providedError }: { providedError: Error | null }) => {
+  if (providedError)
+    return (
+      <div className="hero is-fullheight is-danger">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title is-size-1 has-text-centered is-capitalized ">
+              {providedError.message}
+            </h1>
+            {providedError.message === "Please login or sign up" ? (
+              <div className="buttons is-centered">
+                <Link to="/login" className="button is-link">
+                  Login
+                </Link>
+                <Link to="/signup" className="button is-link">
+                  Signup
+                </Link>
+              </div>
+            ) : null}
+          </div>
+        </div>
+      </div>
+    );
   const error = useRouteError();
   if (!(error instanceof Error)) return null;
   return (
