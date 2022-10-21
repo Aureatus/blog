1e36bcecdc454e52cebd992cdb910b7086ea9a69
Alex Knott <alextknott@gmail.com>
2022-10-21 16:13:04 +0100

    Throw error with response text if response isn't ok

--- src/lib/fetch/getPost.ts
+++ src/lib/fetch/getPost.ts
@@ -1,8 +1,7 @@
 const getPost = async (id: string) => {
   try {
     const response = await fetch(`http://localhost:3000/posts/${id}`);
-    if (!response.ok)
-      throw new Error(`${response.status} ${response.statusText}`);
+    if (!response.ok) throw new Error(await response.text());
     return await response.json();
   } catch (err) {
     if (err instanceof Error) throw err;
