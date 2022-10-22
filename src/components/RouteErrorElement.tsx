8e6d8184b07e373f7f2edfb225148c951bdca76f
Alex Knott <alextknott@gmail.com>
2022-10-22 11:24:52 +0100

    No longer provide conditional rendering
    Custom error rendering won't be used further, as this will just be
    for route errors.

--- src/components/RouteErrorElement.tsx
+++ src/components/RouteErrorElement.tsx
@@ -1,4 +1,4 @@
-import { Link, useRouteError } from "react-router-dom";
+import { useRouteError } from "react-router-dom";
 
 const RouteErrorElement = () => {
   const error = useRouteError();
@@ -10,16 +10,6 @@ const RouteErrorElement = () => {
           <h1 className="title is-size-1 has-text-centered is-capitalized ">
             {error.message}
           </h1>
-          {error.message === "Please login or sign up" ? (
-            <div className="buttons is-centered">
-              <Link to="/login" className="button is-link">
-                Login
-              </Link>
-              <Link to="/signup" className="button is-link">
-                Signup
-              </Link>
-            </div>
-          ) : null}
         </div>
       </div>
     </div>
